ad5f4343a94d7ae0a62fb72c2221a1c6594692cd
david <davidjtulis@gmail.com>
2018-01-03 00:35:52 -0500

    initial commit

--- SharpView/Program.cs
+++ SharpView/Program.cs
@@ -3,10 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.DirectoryServices;
+using System.DirectoryServices.ActiveDirectory;
 
 namespace SharpView {
-    class Program {
-        static void Main(string[] args) {
+    namespace System.DirectoryServices.ActiveDirectory {
+        class Program {
+            static Domain GetDomain(String domainName = "") { //TODO accept user credentials
+                if (!String.IsNullOrEmpty(domainName)) {
+                    var domainContext = new DirectoryContext(DirectoryContextType.Domain, domainName);
+                    return Domain.GetDomain(domainContext);
+                }
+                else {
+                    return Domain.GetCurrentDomain();
+                }
+                
+
+            }
+
+            static void Main(string[] args) {
+
+            }
         }
     }
 }
