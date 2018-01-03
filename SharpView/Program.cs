aa3b90076cd0d86e284f0e19e6d374ac6364925b
david <davidjtulis@gmail.com>
2018-01-03 01:35:27 -0500

    get-domain prototype and readme

--- SharpView/Program.cs
+++ SharpView/Program.cs
@@ -7,23 +7,36 @@ using System.DirectoryServices;
 using System.DirectoryServices.ActiveDirectory;
 
 namespace SharpView {
-    namespace System.DirectoryServices.ActiveDirectory {
-        class Program {
-            static Domain GetDomain(String domainName = "") { //TODO accept user credentials
-                if (!String.IsNullOrEmpty(domainName)) {
-                    var domainContext = new DirectoryContext(DirectoryContextType.Domain, domainName);
-                    return Domain.GetDomain(domainContext);
-                }
-                else {
-                    return Domain.GetCurrentDomain();
-                }
-                
-
+    class SharpView {
+        public static Domain GetDomain(String domainName = "") { //TODO accept user credentials
+            Domain domain = null;
+            if (!String.IsNullOrEmpty(domainName)) {
+                var domainContext = new DirectoryContext(DirectoryContextType.Domain, domainName);
+                domain = Domain.GetDomain(domainContext);
+            }            
+            else {
+                domain = Domain.GetCurrentDomain();
             }
-
-            static void Main(string[] args) {
-
-            }
+            return domain;
         }
     }
 }
+class Program {
+    static void Main(string[] args) {
+        string command = args[0];
+        string argument = args[1];
+        Console.WriteLine("Executing command {0} with arguments {1}", command, argument);
+        Domain domain = null;
+        if (command.ToLower().Equals("getdomain")) {
+            Console.WriteLine("entering function");
+            domain = SharpView.SharpView.GetDomain(argument);
+            Console.WriteLine(domain);
+            Console.WriteLine(domain.DomainControllers[0]);
+            Console.WriteLine(domain.DomainMode);
+            Console.WriteLine(domain.Children[0]);
+            Console.WriteLine(domain.Parent);
+        }
+    }
+}
+    
+
